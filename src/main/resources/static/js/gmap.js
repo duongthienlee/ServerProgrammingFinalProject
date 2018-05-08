427ee11c939f3326715123e81c7a5401e382705b
duongthienly <duongthienlee@gmail.com>
2018-05-08 14:22:12 +0300

    finish styling

--- src/main/resources/static/js/gmap.js
+++ src/main/resources/static/js/gmap.js
@@ -1,6 +1,6 @@
 /*Add Google Maps Function to Set The Map Properties */
 function initMap() {
-    var myLatLng = { lat: 60.197071, lng: 24.926944 };
+    var myLatLng = { lat: 60.2034017, lng: 24.6573263 };
 
     // Create a map object and specify the DOM element for display.
     var map = new google.maps.Map(document.getElementById('googleMap'), {
